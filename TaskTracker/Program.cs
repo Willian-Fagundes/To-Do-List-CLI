45e3c90c8ed7fb853bcced618147d352ea21922a
Willian Fagundes <61697250+Willian-Fagundes@users.noreply.github.com>
2025-12-28 18:06:43 +0000

    inital Commit, first features and db creation

---
+++ TaskTracker/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO.Pipelines;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using models;
+
+bool valid = true;
+var toDo = new ToDoTask();
+string[] Commands = {"add", "update", "delete", "list", "mark-in-progress", "mark-done", "commands"};
+
+
+while(valid == true)
+{
+    Console.WriteLine("--------Task-Tracker--------");
+    string? readResult = Console.ReadLine().ToLower();
+
+    if (readResult != null)
+    {
+        if(Commands.Contains(readResult))
+        {
+            switch (readResult)
+            {
+                case "add":
+
+                    using (var db = new AppDbContext())
+                    {
+                        var NewTask = new ToDoTask
+                        {
+                        Description = readResult.Replace("add",""),
+                        };
+
+                        db.items.Add(NewTask);
+                        db.SaveChanges();
+                        Console.WriteLine("Task Created!");
+                    }
+                    break;
+
+                case "update":
+                    break;
+
+                case "delete":
+                    break;
+
+                case "list":
+                    break;
+
+                case "mark-in-progress":
+                    break;
+
+                case "mark-done":
+                    break;
+                
+                case "commands":
+                    break;
+            }
+        }   
+        else
+        {
+            Console.WriteLine("Error, Command not found. Type 'commands' to see all commands");
+        }
+                
+    }
+}
+
+
---
+++ TaskTracker/models/Models.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace models;
+
+public class ToDoTask
+{
+    public int Id{get;set;}
+    public string Description{get;set;}
+    public string Status{get;set;} = "To-Do";
+    [DataType(DataType.Date)]
+    public DateTime CreatedAt {get;set;} = DateTime.UtcNow;
+    public DateTime UpdatedAt {get;set;} = DateTime.UtcNow;
+}
+
+public class AppDbContext : DbContext
+{
+    public DbSet<ToDoTask> items {get;set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source=tasks.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+    
+}
